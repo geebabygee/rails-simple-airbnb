daf51496800b89b0b1e729b9850657331c9951fb
Irina Popa <irina.oana.popa@gmail.com>
2018-11-12 16:51:35 +0000

    woah

---
+++ app/controllers/flats_controller.rb
@@ -0,0 +1,38 @@
+class FlatsController < ApplicationController
+
+  before_action :set_flat, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @flats = Flat.all
+  end
+
+  def show
+  end
+
+  def new
+    @flat = Flat.new
+  end
+
+  def edit
+  end
+
+  def create
+    @flat = Flat.new(flat_params)
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+    def set_flat
+      @flat = Flat.find(params[:id])
+    end
+
+    def flat_params
+      params.require(:flat).permit(:name, :address, :description, :price_per_night, :number_of_guests, :search)
+    end
+
+end
